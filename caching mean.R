0a3789e5c4e2c1959b6d3aaacd82bbcddd8f3a40
souravkd1502 <49359961+souravkd1502@users.noreply.github.com>
2021-06-29 20:35:59 +0530

    Add files via upload

---
+++ caching mean.R
@@ -0,0 +1,14 @@
+## caching the mean of a vector
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
